c4305612a2bbeac8dbb85443ed8cc784fb8caf1b
Yash Tripathi <yashtripathi0905@gmail.com>
2026-06-04 01:47:13 +0530

    Enhance localization: Update English and Japanese language files for improved clarity and consistency across various user messages and validation prompts.

--- resources/lang/ja/auth.php
+++ resources/lang/ja/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'ログインIDまたはパスワードが正しくありません。',
+    'throttle' => 'ログインの試行回数が多すぎます。:seconds秒後にもう一度お試しください。',
 
 ];
--- resources/lang/ja/passwords.php
+++ resources/lang/ja/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'パスワードが再設定されました！',
+    'sent' => 'パスワード再設定リンクを送信しました！',
+    'throttled' => '時間を置いてから再度お試しください。',
+    'token' => 'このパスワードリセットトークンは無効です。',
+    'user' => "このメールアドレスを持つユーザーが見つかりません。",
 
 ];
